52ed4a572965c5ac03c46feb14ab40fae35754ac
Manuel Maisano <manumaisano@gmail.com>
2024-04-16 18:00:47 +0200

    headercomponent +generals and variables scss

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,5 +1,8 @@
 <template>
-   
+   <div class="d-flex align-items-center bg-white">
+    <img id="logo" class="img-fluid" src="https://upload.wikimedia.org/wikipedia/commons/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg" alt="Logo">
+    <h1>Yu-Gi-Oh Api</h1>
+   </div>
 </template>
 
 <script>
@@ -8,4 +11,22 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+
+<style lang="scss" scoped>
+
+#logo{
+    width: 200px;
+    padding: 20px;
+
+}
+  h1{
+    font-weight: 400;
+    font-size: 55px;
+    
+   
+  }
+
+
+
+
+</style>
