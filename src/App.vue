d73b7fd9645ed129c5bfa2640aa2380c91a3f8c8
Manuel Maisano <manumaisano@gmail.com>
2024-04-16 18:13:35 +0200

    create Maincomponent

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <HeaderComponent />
+  <MainComponent />
 </template>
 
 <script>
 import { store } from './store';
 import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
   export default {
     name: 'App',
     components:{
       HeaderComponent,
+      MainComponent,
     },
     data(){
       return{
