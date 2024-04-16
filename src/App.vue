d2b4d86979ed526bfef2948a141f3ef95224b5f0
Manuel Maisano <manumaisano@gmail.com>
2024-04-16 18:00:59 +0200

    add header component

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@ import { store } from './store';
 import HeaderComponent from './components/HeaderComponent.vue';
   export default {
     name: 'App',
+    components:{
+      HeaderComponent,
+    },
     data(){
       return{
         store
