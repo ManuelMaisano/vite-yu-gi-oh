71e09da951c01a70e484c2980fbe49775b588456
Manuel Maisano <manumaisano@gmail.com>
2024-04-16 18:03:07 +0200

    formdocument styles header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,8 +1,9 @@
 <template>
-   <div class="d-flex align-items-center bg-white">
-    <img id="logo" class="img-fluid" src="https://upload.wikimedia.org/wikipedia/commons/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg" alt="Logo">
-    <h1>Yu-Gi-Oh Api</h1>
-   </div>
+    <div class="d-flex align-items-center bg-white">
+        <img id="logo" class="img-fluid"
+            src="https://upload.wikimedia.org/wikipedia/commons/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg" alt="Logo">
+        <h1>Yu-Gi-Oh Api</h1>
+    </div>
 </template>
 
 <script>
@@ -13,20 +14,15 @@ export default {
 
 
 <style lang="scss" scoped>
-
-#logo{
+#logo {
     width: 200px;
     padding: 20px;
 
 }
-  h1{
+
+h1 {
     font-weight: 400;
     font-size: 55px;
-    
-   
-  }
 
-
-
-
+}
 </style>
