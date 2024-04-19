12f0090d93823189a6e205e79d9a3e9c7ab49b94
Manuel Maisano <manumaisano@gmail.com>
2024-04-19 17:18:38 +0200

    change title and fix cardtext

--- src/components/MainComponent/CardComponent.vue
+++ src/components/MainComponent/CardComponent.vue
@@ -1,28 +1,34 @@
 <template>
-   <div class="card" style="width: 18rem;">
-        <img class="card-img-top" :src="image" :alt="name">
-        <div class="card-body">
-          <p class="card-text">
-            <span>{{ name }}</span>
-            <br>
-            <span>{{ archetype }}</span>
-          </p>
-        </div>
-      </div>
+  <div class="card" style="width: 18rem;">
+    <img class="card-img-top" :src="image" :alt="name">
+    <div class="card-body">
+      <p class="card-text text-center info">
+        <span>{{ name }}</span>
+        <br>
+        <span>{{ archetype }}</span>
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'CardComponent',
-        props: [
-        'id',
-            'image',
-            'name',
-            'archetype'
-        ],
-    }
+export default {
+  name: 'CardComponent',
+  props: [
+    'id',
+    'image',
+    'name',
+    'archetype'
+  ],
+}
 </script>
 
 <style lang="scss" scoped>
+.info {
+  background-color: #D48F38;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 
+}
 </style>
